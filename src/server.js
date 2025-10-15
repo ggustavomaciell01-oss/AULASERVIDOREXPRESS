817b3ed924aa35d5da05e96476f469723d913264
VitorBernalRodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-10-15 16:32:20 -0400

    aula 8/9

--- src/server.js
+++ src/server.js
@@ -1,30 +1,32 @@
 import express from "express";
+import { Database } from "./databases.js";
 
 const app = express();
 app.use(express.json())
 
-const usuarios = []
+const database = new Database()
 
 app.get('/', (req, res) => {
     res.send("Voce esta na pagina principal get")
 })
 
+
+
 app.get('/', (req, res) => {
-    res.status(200).json(usuarios)
+    const data = database.select("usuario")
+    res.status(200).json()
 })
 
 app.post('/usuario', (req, res) => {
     const {nome, idade, email} = req.body
-    if(nome, idade, email){
-        usuarios.push({nome, idade})
-        return res.status(201).send("Usuario Criado")
-
-
+    if (nome, idade, email){
+        database.insert('usuario')
+        usuarios.push({nome, idade, email})
+        res.status(201).send("Usuario Criado")
     }
 
-    return res.status(400).send("Informacoes invalidas")
-
-    res.send("Voce esta na pagina principal post")
+    return res.status(400).send("informações invalidas")
+    
 }) 
 
 app.listen(3333, () => {
